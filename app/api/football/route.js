8bd79b806e375ef3e966c7dd397923b079a39a93
youssefbenkoayb42-art <youssefbenkoayb42@gmail.com>
2026-08-18 19:57:07 +0100

    Add football API route

--- app/api/football/route.js
+++ app/api/football/route.js
@@ -1,1 +1,15 @@
 
+export async function GET() {
+  const response = await fetch(
+    "https://v3.football.api-sports.io/fixtures?next=10",
+    {
+      headers: {
+        "x-apisports-key": process.env.API_FOOTBALL_KEY,
+      },
+    }
+  );
+
+  const data = await response.json();
+
+  return Response.json(data);
+}
