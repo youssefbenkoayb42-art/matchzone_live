679386b98dd58412c2d17b178a445f00bd2e4902
youssefbenkoayb42-art <youssefbenkoayb42@gmail.com>
2026-08-20 20:25:10 +0100

    feat: add match details page

---
+++ app/matches/[id]/page.js
@@ -0,0 +1,8 @@
+export default function MatchPage() {
+  return (
+    <main style={{ padding: "40px", textAlign: "center" }}>
+      <h1>تفاصيل المباراة</h1>
+      <p>جاري تحميل معلومات المباراة...</p>
+    </main>
+  );
+}
