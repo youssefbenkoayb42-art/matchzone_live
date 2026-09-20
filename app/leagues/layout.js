586f9cd57e35639ab05c1ce78f9bda1fdafd5a24
youssefbenkoayb42-art <youssefbenkoayb42@gmail.com>
2026-09-20 17:34:45 +0100

    Add league section metadata

---
+++ app/leagues/layout.js
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: "البطولات",
+  description: "تصفح أهم بطولات كرة القدم ومباريات ونتائج الدوري الإنجليزي والإسباني والإيطالي والألماني والفرنسي.",
+};
+export default function LeaguesLayout({ children }) {
+  return children;
+}
