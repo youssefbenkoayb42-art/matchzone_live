6a195bb8a291166979149fd61823158b8f573080
youssefbenkoayb42-art <youssefbenkoayb42@gmail.com>
2026-09-20 17:34:47 +0100

    Add match detail metadata

---
+++ app/matches/[id]/layout.js
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: "تفاصيل المباراة",
+  description: "تفاصيل ونتيجة مباراة كرة القدم ومعلومات الفريقين والملعب وأبرز الأحداث.",
+};
+export default function MatchLayout({ children }) {
+  return children;
+}
